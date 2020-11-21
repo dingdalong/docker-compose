fd7d55a507c6e0f115502efe1125f8eb3eee8790
dingdalong <330373763@qq.com>
2020-11-21 16:23:07 +0800

    调整

--- ftp/inotify-tools/inotify_bak.sh
+++ ftp/inotify-tools/inotify_bak.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-src=/home/develop/ftp/vsftpd/ewan
+src=/home/develop/ftp/vsftpd/<account>
 /usr/local/bin/inotifywait -mrq --timefmt '%d/%m/%y %H:%M' --format '%T %w%f%e' -e close_write,delete,create,attrib $src |  while read file
 do
-    /usr/bin/rsync -arzuq $src ewan@192.168.130.71::helloRsync --password-file=/etc/rsync.password
+    /usr/bin/rsync -arzuq $src <account>@192.168.130.71::helloRsync --password-file=/etc/rsync.password
     echo "  ${file} was rsynced" >>/var/log/rsync.log 2>&1
 done
