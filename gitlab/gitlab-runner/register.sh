28d0e47eb14604731e5b41d9efd77ac56154d61f
dingdalong <330373763@qq.com>
2021-01-22 18:10:22 +0800

    gitlab-runner

--- gitlab/gitlab-runner/register.sh
+++ gitlab/gitlab-runner/register.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker exec -it $3 gitlab-runner register --non-interactive --url "$1" --registration-token "$2" --executor "shell" --run-untagged
+docker exec -it $3 gitlab-runner register --non-interactive --url "$1" --registration-token "$2" --executor "shell" --description "$3" --run-untagged
