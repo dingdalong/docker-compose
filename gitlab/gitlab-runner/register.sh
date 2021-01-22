aeee71afc0156bc5d454b4c6fe425a2165cdb54b
dingdalong <330373763@qq.com>
2021-01-22 20:02:00 +0800

    让centos容器运行gitlab-runner服务

--- gitlab/gitlab-runner/register.sh
+++ gitlab/gitlab-runner/register.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 docker exec -it $3 gitlab-runner register --non-interactive --url "$1" --registration-token "$2" --executor "shell" --description "$3" --run-untagged
+docker exec -it $3 gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+docker exec -it $3 gitlab-runner start
